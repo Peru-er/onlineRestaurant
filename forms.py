61c99704085b61e0aad8706c1a9e1a7e07d6fd46
Peru-er <shynkar.snizhana@gmail.com>
2025-07-16 14:37:17 +0200

    Initial commit

--- forms.py
+++ forms.py
@@ -1,5 +1,13 @@
 
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, FloatField, FileField, IntegerField, HiddenField
+from wtforms.validators import DataRequired, Email, EqualTo, NumberRange
+
+class RegisterForm(FlaskForm):
+    nickname = StringField('Nickname', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FloatField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo
 
@@ -22,4 +30,35 @@ class AddPositionForm(FlaskForm):
     description = StringField('Description', validators=[DataRequired()])
     price = FloatField('Price', validators=[DataRequired()])
     weight = FloatField('Weight', validators=[DataRequired()])
+    category = StringField('Category', validators=[DataRequired()])
     submit = SubmitField('Add')
+
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    nickname = StringField('Nickname', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class AddPositionForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    img = FileField('Image', validators=[DataRequired()])
+    ingredients = StringField('Ingredients', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    weight = FloatField('Weight', validators=[DataRequired()])
+    category = StringField('Category', validators=[DataRequired()])
+    submit = SubmitField('Add')
+
+class AddToCartForm(FlaskForm):
+    name = HiddenField(validators=[DataRequired()])
+    num = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1)])
+    submit = SubmitField('Add')
+
+class OrderForm(FlaskForm):
+    submit = SubmitField('Place Order')
+
+class DummyForm(FlaskForm):
+    pass
+
--- online_restaurant.py
+++ online_restaurant.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, abort
 
 from flask_login import LoginManager, login_required, current_user, login_user, logout_user
 
@@ -9,10 +9,11 @@ from datetime import datetime
 import secrets
 from flask_wtf import CSRFProtect
 
+from dotenv import load_dotenv
 import os
 import uuid
 
-from forms import RegisterForm, LoginForm, AddPositionForm
+from forms import RegisterForm, LoginForm, AddPositionForm, AddToCartForm, OrderForm, DummyForm
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -21,6 +22,9 @@ csrf = CSRFProtect(app)
 
 FILES_PATH = 'static/menu'
 
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['UPLOAD_FOLDER'] = FILES_PATH
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
 app.config['MAX_FORM_PARTS'] = 500
@@ -105,9 +109,6 @@ def login():
 
     return render_template('login.html', form=form)
 
-
-
-
 @app.route('/logout/')
 @login_required
 def logout():
@@ -122,6 +123,10 @@ def add_position():
         return redirect(url_for('home'))
 
     form = AddPositionForm()
+    print('FORM SUBMIT:', request.method)
+    print('IS VALID:', form.validate_on_submit())
+    print('FORM ERRORS:', form.errors)
+
     if form.validate_on_submit():
         file = form.img.data
         filename = str(uuid.uuid4()) + os.path.splitext(file.filename)[1]
@@ -134,6 +139,7 @@ def add_position():
             ingredients=form.ingredients.data,
             description=form.description.data,
             price=form.price.data,
+            category=form.category.data,
             file_name=filename
         )
 
@@ -146,6 +152,141 @@ def add_position():
         return redirect(url_for("home"))
 
     return render_template('add_position.html', form=form)
+
+@app.route('/menu/')
+def menu():
+    with Session() as session:
+        all_positions = session.query(Menu).filter_by(active=True).all()
+    return render_template('menu.html', all_positions=all_positions)
+
+@app.route('/position/<name>', methods=['GET', 'POST'])
+def position(name):
+    form = AddToCartForm()
+
+    with Session() as cursor:
+        us_position = cursor.query(Menu).filter_by(active=True, name=name).first()
+        if not us_position:
+            abort(404)
+
+    form.name.data = us_position.name
+
+    if form.validate_on_submit():
+        position_name = form.name.data
+        position_num = form.num.data
+
+        basket = session.get('basket', {})
+        basket[position_name] = position_num
+        session['basket'] = basket
+
+        flash('Position added to the basket.', 'success')
+        return redirect(url_for('position', name=position_name))
+
+    return render_template('position.html', form=form, position=us_position)
+
+
+@app.route('/basket/')
+def basket():
+    basket = session.get('basket', {})
+
+    if not basket:
+        flash("Your basket is currently empty.", "info")
+        return redirect(url_for('menu'))
+
+    items = []
+    total = 0
+
+    with Session() as db:
+        for name, quantity in basket.items():
+            position = db.query(Menu).filter_by(name=name).first()
+            if position:
+                subtotal = position.price * int(quantity)
+                total += subtotal
+                items.append({
+                    'name': name,
+                    'quantity': quantity,
+                    'price': position.price,
+                    'subtotal': subtotal,
+                    'img': position.file_name
+                })
+
+    form = DummyForm()
+
+    return render_template('basket.html', items=items, total=total, form=form)
+
+
+@app.route('/create_order/', methods=['GET', 'POST'])
+def create_order():
+    form = OrderForm()
+    basket = session.get('basket')
+
+    if form.validate_on_submit():
+        if not current_user.is_authenticated:
+            flash("To place an order you must be registered.")
+        elif not basket:
+            flash("Your basket is empty.")
+        else:
+            with Session() as cursor:
+                new_order = Orders(order_list=basket, order_time=datetime.now(), user_id=current_user.id)
+                cursor.add(new_order)
+                cursor.commit()
+                session.pop('basket')
+                cursor.refresh(new_order)
+                return redirect(f"/my_order/{new_order.id}")
+
+    return render_template('create_order.html', form=form, basket=basket)
+
+@app.route('/clear_basket/', methods=['POST'])
+def clear_basket():
+    session.pop('basket', None)
+    flash("Basket cleared.")
+    return redirect(url_for('basket'))
+
+@app.route('/update_quantity', methods=['POST'])
+def update_quantity():
+    item_name = request.form.get('item_name')
+    action = request.form.get('action')
+
+    basket = session.get('basket', {})
+
+    warning_occurred = False
+
+    if item_name in basket:
+        if action == 'increase':
+            if basket[item_name] < 8:
+                basket[item_name] += 1
+            else:
+                flash("Cannot increase above 8", 'warning')
+                warning_occurred = True
+        elif action == 'decrease':
+            if basket[item_name] > 1:
+                basket[item_name] -= 1
+            else:
+                flash("Cannot decrease below 1. To remove item use 'Delete'.", 'warning')
+                warning_occurred = True
+
+        session['basket'] = basket
+
+        if not warning_occurred:
+            flash(f'Quantity updated for {item_name}', 'success')
+    else:
+        flash(f'{item_name} not found in basket.', 'error')
+
+    return redirect(url_for('basket'))
+
+
+@app.route('/remove_item/', methods=['POST'])
+def remove_item():
+    item_name = request.form.get('item_name')
+    basket = session.get('basket', {})
+
+    if item_name in basket:
+        del basket[item_name]
+        session['basket'] = basket
+        flash(f'{item_name} removed from the basket.', 'success')
+    else:
+        flash(f'{item_name} not found in basket.', 'error')
+
+    return redirect(url_for('basket'))
 
 
 if __name__ == '__main__':
